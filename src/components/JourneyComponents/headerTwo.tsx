c1da7243b5b66b86d3a62b8f1d37dd70c33e1869
DzawilUqul <dzawilu@gmail.com>
2025-02-01 21:27:19 +0700

    fix: import HeaderTwo

--- src/components/JourneyComponents/headerTwo.tsx
+++ src/components/JourneyComponents/headerTwo.tsx
@@ -1,7 +1,7 @@
-import type { headerTwo } from '@/types/ContentTypes'
+import type { HeaderTwo } from '@/types/ContentTypes'
 import React from 'react'
 
-const HeaderTwo: React.FC<headerTwo> = ({ title }) => {
+const HeaderTwo: React.FC<HeaderTwo> = ({ title }) => {
   return (
     <h2 className="h2 underline mt-10">{title}</h2>
   )
